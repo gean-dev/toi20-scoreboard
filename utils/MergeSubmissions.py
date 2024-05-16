525bf11e21c09c21acfe011bb20093f1375825a2
ttamx <teetat.tata@gmail.com>
2024-05-16 13:11:58 +0700

    feat: add day 2

--- utils/MergeSubmissions.py
+++ utils/MergeSubmissions.py
@@ -1,11 +1,11 @@
 import json
 import os
 
-def removeNotice(filePath):
+def mergeSubmissions(filePath):
     submissions1 = json.load(open('sublist/' + filePath))
     submissions2 = json.load(open('sublist2/' + filePath))
     submissions = submissions1 + submissions2
     json.dump(submissions, open('sublist/' + filePath, 'w'))
 
 for file in os.listdir('sublist'):
-    removeNotice(file)
+    mergeSubmissions(file)
